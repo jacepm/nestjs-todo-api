ae1fa5282a4d1e6720ed7ee9cc9a6b3e270f5119
Jc <jcpmandawe@gmail.com>
2021-06-17 11:13:26 +0800

    Add mongodb module and options in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from "@mayajs/core";
 import { RouterModule } from "@mayajs/router";
+import { MongoDbModule } from "@mayajs/mongo";
 import { routes } from "./app.routing.module";
 import { AppController } from "./app.controller";
 
+export const mongoOptions = {
+  connectionString: "mongodb://127.0.0.1:27017",
+  name: "test",
+  options: { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false },
+  schemas: [],
+};
+
 @Module({
   declarations: [AppController],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), MongoDbModule.forRoot(mongoOptions)],
   providers: [],
   bootstrap: AppController,
 })
