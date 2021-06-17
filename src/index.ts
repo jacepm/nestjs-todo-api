d087615f4299a4ad9d77be56d0de68dfc0d6e919
Jc <jcpmandawe@gmail.com>
2021-06-17 12:02:53 +0800

    Add body-parser plugins

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { configServer } from "@mayajs/core";
 import cors from "cors";
+import parser from "body-parser";
 import { AppModule } from "./app.module";
 
-configServer(3333).usePlugins([cors()]).bootstrapModule(AppModule);
+const plugins = [cors(), parser.urlencoded({ extended: false }), parser.json()];
+configServer(3333).usePlugins(plugins).bootstrapModule(AppModule);
