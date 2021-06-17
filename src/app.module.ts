cd446f481dd96300cefc92444ca84fc315262a24
Jc <jcpmandawe@gmail.com>
2021-06-17 11:24:05 +0800

    Add todos in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,21 @@ import { RouterModule } from "@mayajs/router";
 import { MongoDbModule } from "@mayajs/mongo";
 import { routes } from "./app.routing.module";
 import { AppController } from "./app.controller";
+import { TodosController } from "./todos/todos.controller";
+import { TodosServices } from "./todos/todos.service";
+import todos from "./todos/todos.model";
 
 export const mongoOptions = {
   connectionString: "mongodb://127.0.0.1:27017",
   name: "test",
   options: { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false },
-  schemas: [],
+  schemas: [todos],
 };
 
 @Module({
-  declarations: [AppController],
+  declarations: [AppController, TodosController],
   imports: [RouterModule.forRoot(routes), MongoDbModule.forRoot(mongoOptions)],
-  providers: [],
+  providers: [TodosServices],
   bootstrap: AppController,
 })
 export class AppModule {}
