b67c0c7da783d7327976bc9160d02ac8863be3e8
Jc Mandawe <jcpmandawe@gmail.com>
2021-10-04 09:27:58 +0800

    refactor: remove mayajs

--- src/app.routing.module.ts
+++
@@ -1,8 +0,0 @@
-import { TodosController } from "./todos/todos.controller";
-
-export const routes = [
-  {
-    path: "todos",
-    controller: TodosController,
-  },
-];
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { configServer } from "@mayajs/core";
-import cors from "cors";
-import parser from "body-parser";
-import { AppModule } from "./app.module";
-
-const plugins = [cors(), parser.urlencoded({ extended: false }), parser.json()];
-configServer(3333).usePlugins(plugins).bootstrapModule(AppModule);
